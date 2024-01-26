6d39512a5bb39ebd113d64603934e5d466c2cee9
KeiMT <kei0311.university@gmail.com>
2024-01-27 03:03:39 +0900

    セリフ、キャラクター・背景などの表示に成功

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -2,8 +2,11 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import Image from "next/image"
-import class_image from '/public/class_day.jpg';
+import Image, { StaticImageData } from "next/image"
+import emp from '/public/emp.png';
+import chara1_normal from '/public/chara1-normal.png';
+import chara1_evilsmile from '/public/chara1-evilsmile.png';
+import chara1_smile from '/public/chara1-smile.png';
 
 
 
@@ -13,9 +16,27 @@ const GamePage: React.FC = () => {
   const { name } = router.query;
   const [showConfirmation, setShowConfirmation] = useState(false);
 
+  // セリフ番号
+  const [ind, setInd] = useState(0);
+  const [indc, setIndc] = useState(0);
+  const [textToShow, setTextToShow] = useState("Welcome to the game!");
+  const [charaToShow, setCharaToShow] = useState(emp);
+
+  // セリフ集...
+  const texts: string[] = ["おはよう、" + name + "!", "こんにちは、" + name, "こんばんは、" + name];
+  const charas: StaticImageData[] = [chara1_normal, chara1_evilsmile, chara1_smile];
+
   const handleConfirmationButtonClick = () => {
     // ここで確認ウィンドウを表示するための処理を追加
     setShowConfirmation(true);
+  };
+
+  const textClick = () => {
+    // テキストがクリックされたかどうか
+    setInd((prevInd) => (prevInd + 1) % texts.length);
+    setTextToShow(texts[ind]);
+    setCharaToShow(charas[ind]);
+    console.log("クリック！" + ind);
   };
 
 
@@ -24,38 +45,48 @@ const GamePage: React.FC = () => {
     <main>
 
 
-      <div className='text_body'>
-        <div id="imageContainer">
+
+      <div className='overlay-base'>
+        <div className='image_back'>
+
+          <Image className="overlay-image" src={charaToShow} alt="Overlay Image" />
+
+          <div className="center-text">
 
 
-          <Image className='image_back' src={class_image} alt="class_image" />
 
-          <div id="textOverlay">
-            <h1>Welcome to the game, {name}!</h1>
-            <button type="button" className="select_button" onClick={handleConfirmationButtonClick}>
-              めにゅー
-            </button>
-
-            {showConfirmation && (
-              <div className="confirmation_modal">
-                <p>めにゅー</p>
-                <button className='menu_button'>せーぶ</button>
-                <button className='menu_button'>ろーど</button>
-                <Link href="/option">
-                  <button className='menu_button'>おぷしょん</button>
-                </Link>
-                <Link href="/..">
-                  <button className='menu_button'>たいとるにもどる</button>
-                </Link>
-                <button className='menu_button' onClick={() => setShowConfirmation(false)}>めにゅーをとじる</button>
-              </div>
-            )}
+            <p onClick={textClick} className='main-text'>{textToShow}</p>
 
 
-            <p className='text'>Welcome to the game, {name}!</p>
+            <div className='menu'>
+              <button type="button" className="select_button" onClick={handleConfirmationButtonClick}>
+                めにゅー
+              </button>
+
+              {showConfirmation && (
+                <div className="confirmation_modal">
+                  <p>めにゅー</p>
+                  <button className='menu_button'>せーぶ</button>
+                  <button className='menu_button'>ろーど</button>
+                  <Link href="/option">
+                    <button className='menu_button'>おぷしょん</button>
+                  </Link>
+                  <Link href="/..">
+                    <button className='menu_button'>たいとるにもどる</button>
+                  </Link>
+                  <button className='menu_button' onClick={() => setShowConfirmation(false)}>めにゅーをとじる</button>
+                </div>
+              )}
+            </div>
+
           </div>
         </div>
+
       </div>
+
+
+
+
 
     </main>
   );
