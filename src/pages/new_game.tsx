50e0cc0ac064972f32cfb1b37e91fadd1ad1886b
KeiMT <kei0311.university@gmail.com>
2024-02-18 18:03:37 +0900

    new_gameでの名前の入力条件を修正しました。

--- src/pages/new_game.tsx
+++ src/pages/new_game.tsx
@@ -5,10 +5,24 @@ import Link from 'next/link';
 const NewGame: React.FC = () => {
   const [name, setName] = useState('');
   const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showAlertS, setShowAlertS] = useState(false);
+  const [showAlertL, setShowAlertL] = useState(false);
 
   const handleConfirmationButtonClick = () => {
     // ここで確認ウィンドウを表示するための処理を追加
-    setShowConfirmation(true);
+    if (name.length < 1) {
+      setShowAlertS(true);
+      // setShowConfirmation(false);
+    }
+    else if (name.length > 10) {
+      setShowAlertL(true);
+      // setShowConfirmation(false);
+    }
+    else {
+      setShowConfirmation(true);
+      // setShowAlert(false);
+    }
+
   };
 
   const handleConfirmSubmit = () => {
@@ -20,38 +34,67 @@ const NewGame: React.FC = () => {
 
   return (
     <main>
-      <h1 className='h1_1'>にゅうげーむ🎮</h1>
+      <div className='overlay-base'>
+        <div className='image_back_new_game'>
+          <div className="center-text">
+            <h1 className='h1_1'>にゅうげーむ🎮</h1>
+            <label>あなたのなまえは？</label>
+            <input
+              disabled={showAlertS || showAlertL || showConfirmation}
+              className="input_text"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="なまえをにゅうりょくしてください"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <br />
+            <button type="button" className="select_button" onClick={handleConfirmationButtonClick}>
+              けってい
+            </button>
+
+            {showConfirmation && (
+              <div className='load'>
+                <div className="confirmation_modal">
+                  <p>{name} でよろしいですか？</p>
+                  <Link className='link' href={`/game?name=${encodeURIComponent(name)}`}>
+                    <button className='yes_button'>はい</button>
+                  </Link>
+                  <button className='no_button' onClick={() => setShowConfirmation(false)}>いいえ</button>
+                </div>
+              </div>
+            )}
+
+            {showAlertS && (
+              <div className='load'>
+                <div className="confirmation_modal">
+                  <p>名前を入力してください！</p>
+                  <button className='menu_button' onClick={() => setShowAlertS(false)}>とじる</button>
+                </div>
+              </div>
+            )}
+            {showAlertL && (
+              <div className='load'>
+                <div className="confirmation_modal">
+                  <p>名前が長すぎます！</p>
+                  <p>10文字以下にしてください</p>
+                  <button className='menu_button' onClick={() => setShowAlertL(false)}>とじる</button>
+                </div>
+              </div>
+            )}
 
 
-      <label>あなたのなまえは？</label>
-      <input
-        className="input_text"
-        type="text"
-        id="name"
-        name="name"
-        placeholder="なまえをにゅうりょくしてください"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        required
-      />
-      <br />
-      <button type="button" className="select_button" onClick={handleConfirmationButtonClick}>
-        けってい
-      </button>
 
-      {showConfirmation && (
-        <div className="confirmation_modal">
-          <p>{name} でよろしいですか？</p>
-          <Link href={`/game?name=${encodeURIComponent(name)}`}>
-            <button className='yes_button'>はい</button>
-          </Link>
-          <button className='no_button' onClick={() => setShowConfirmation(false)}>いいえ</button>
+            <Link className='menu_button' href="/..">たいとるにもどる</Link>
+          </div>
         </div>
-      )}
+      </div>
 
 
 
-      <Link className='menu_button' href="/..">たいとるにもどる</Link>
+
     </main>
   );
 }
